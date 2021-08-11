4dbd3116b012d377301d1f5b9e4d51e17d174176
Anna Tsybel <anna.tsybel@nure.ua>
2021-08-11 16:08:31 +0300

    Hunger people first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,51 @@
-const toggleBtn = document.querySelector("#toggle-theme");
-toggleBtn.addEventListener("click", function() {
-    if(document.documentElement.hasAttribute("theme")){
-        document.documentElement.removeAttribute("theme");
+/*ANCHORS*/
 
-    }
-    else{
-        document.documentElement.setAttribute("theme", "dark");
-    }
-});
+const anchor = document.querySelectorAll('ul li a[href*="#"],a.header__scroll[href*="#"]');
+for (let key of anchor) {
+    key.addEventListener('click', (e) => {
+        e.preventDefault();
+        const blockId = key.getAttribute('href');
+        document.querySelector('' + blockId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
 
-function buttonPopup() {
-        alert("Hello");
-      
+    })
+
+}
+
+
+/** @menu */
+
+let menuList = document.querySelector('.menu');
+let menuCategories = document.querySelectorAll('.menu-categories ul li button');
+let menu = {
+    'soupe': soupe,
+    'pizza': pizza,
+    'pasta': pasta,
+    'desert': desert,
+    'wine': wine,
+    'beer': beer,
+    'drinks': drinks
+}
+for (let keys of menuCategories) {
+    keys.addEventListener('click', (e) => {
+        let changeMenu = function(object) {
+            menuList.innerHTML = '';
+            for (let i = 0; i < object.length; i++) {
+                let currentMenu = new Menulist(object[i]);
+                let menuElement = document.createElement("div");
+                menuElement.classList.add('menu__item');
+                menuElement.innerHTML = `<p>${currentMenu.getFirstRow()}<span>${currentMenu.getSecondRow()}</span></p>`
+                menuList.appendChild(menuElement);
+            }
+
+        }
+        for (let [key, value] of Object.entries(menu)) {
+            if (key === keys.getAttribute('id')) {
+                changeMenu(value);
+            }
+        }
+    })
+
 }
---
+++ app/js/menu-collection.js
@@ -0,0 +1,191 @@
+class Menulist {
+    constructor(options) {
+        this.name = options.name;
+        this.price = options.price;
+        this.text = options.text;
+    }
+    getFirstRow() {
+        return this.name + '. . . ' + this.price + ' USD';
+    }
+    getSecondRow() {
+        return this.text;
+    }
+}
+
+let wine = [{
+        name: 'wine',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'wine',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'wine',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'wine',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'wine',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let pasta = [{
+        name: 'pasta',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pasta',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pasta',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pasta',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pasta',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let desert = [{
+        name: 'desert',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'desert',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'desert',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'desert',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'desert',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let beer = [{
+        name: 'beer',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'beer',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'beer',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'beer',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'beer',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let drinks = [{
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'drinks',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let pizza = [{
+        name: 'pizza',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pizza',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pizza',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pizza',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'pizza',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
+let soupe = [{
+        name: 'soupe',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'soupe',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'soupe',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'soupe',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }, {
+        name: 'soupe',
+        price: '55.14',
+        text: 'Integer ullamcorper neque eu purus euismod'
+    }
+
+]
